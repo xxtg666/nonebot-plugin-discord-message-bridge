c4f0e3416aa911ecba398f004710b15280c6f60a
XiaoDeng3386 <104149371+This-is-XiaoDeng@users.noreply.github.com>
2024-06-28 08:03:56 +0000

    修改 config 和 README.md

--- README.md
+++ README.md
@@ -20,7 +20,56 @@
 
 ## 💿 安装
 
-目前只提供了 GitHub 下载源码安装方式
+### 1. 创建一个新的 NoneBot2 机器人
+
+> 如果您需要将本插件安装到现有的机器人，可忽略此步骤
+
+注意在 `pyproject.toml` 中配置一个 `plugin_dirs`，可以参考以下设置：
+
+```toml
+plugin_dirs = ["src/plugins"]
+```
+
+> [!TIP]
+> 在配置完成后您需要在机器人工作目录新建此文件夹，以下内容中将用 `插件目录` 代之这一目录。
+
+### 2. 安装插件
+
+您可以选择以下两种安装方式
+
+<details>
+<summary>使用 Git 子模块安装</summary>
+
+> [!TIP]
+> 此方法需要您能够使用 git 并已经在机器人目录下初始化 git 仓库
+
+请将以下指令的 `src/plugins/` 替换为您的插件目录
+
+```bash
+git submodule add https://github.com/xxtg666/nonebot-plugin-discord-message-bridge src/plugins/nonebot_plugin_discord_message_bridge
+git submodule update --init --recursive
+```
+
+</details>
+
+
+<details>
+<summary>使用文件</summary>
+
+1. [下载本仓库](https://github.com/xxtg666/nonebot-plugin-discord-message-bridge/archive/refs/heads/main.zip)
+2. 将 `nonebot-plugin-discord-message-bridge-main` 文件夹解压到插件目录，并重命名为 `nonebot_plugin_discord_message_bridge`
+
+</details>
+
+### 3. 安装依赖
+
+经过步骤二后，您的插件应该被安装到了 `插件目录/nonebot_plugin_discord_message_bridge` 中，本步骤您需要进入 `插件目录/nonebot_plugin_discord_message_bridge` 安装本插件所需依赖
+
+```bash
+pip install -r requirements.txt
+```
+
+如果您使用了其他包管理器，请参考您所使用的包管理器的使用方法安装所需依赖
 
 ## ⚙️ 配置
 
--- config.py
+++ config.py
@@ -1,54 +1,81 @@
+from pydantic import BaseModel
+from nonebot import get_plugin_config
+
+
+class Config(BaseModel):
+    token: str
+    webhook_url: str
+    webhook_id: int
+    guild_id: int
+    channel_id: int
+    qq_id: int
+    log_file: str = "data/dmb_log.txt"
+    qq_bind_file: str = "data/discord_message_bridge_qq_bind.json"
+    qq_bind_file_2: str = "data/discord_message_bridge_qq_bind_2.json"
+    qq_forward_failed: str = "cry"
+    max_reply_preview_length: int = 100
+    http_proxy: str = "http://127.0.0.1:7890"
+    prefix: str = "[Discord] "
+    bot_name: str = "Discord Message Bridge"
+    qq_sudo_format: str = "/sudo {Q} {C}"
+    qq_command: str = "dmb"
+    discord_command_prefix: str = "~"
+    qq_command_prefix: str  = "."
+
+config = get_plugin_config(Config)
+
+
 # 网络代理地址，若不需要请留空
-HTTP_PROXY = "http://127.0.0.1:7890"
+HTTP_PROXY = config.http_proxy
 
 # Discord Bot Token
-TOKEN = "xxxxxxxxxx"
+TOKEN = config.token
 
 # Discord Webhook URL
-WEBHOOK_URL = "https://discord.com/api/webhooks/xxxxxxxxxx/xxxxxxxxxx"
+WEBHOOK_URL = config.webhook_url
 
 # Discord Webhook ID (WEBHOOK_URL 中的那一串数字)
-WEBHOOK_ID = 0
+WEBHOOK_ID = config.webhook_id
 
 # Discord 服务器 ID
-GUILD_ID = 0
+GUILD_ID = config.guild_id
 
 # Discord 频道 ID
-CHANNEL_ID = 0
+CHANNEL_ID = config.channel_id
 
 # QQ 群号
-QQ_ID = 0
+QQ_ID = config.qq_id
 
 # 转发失败时添加的 reaction 名称 (支持自定义表情符号)
-QQ_FORWARD_FAILED = "cry"
+QQ_FORWARD_FAILED = config.qq_forward_failed
 
 # 日志文件路径
-LOG_FILE = "data/dmb_log.txt"
+LOG_FILE = config.log_file
 
 # QQ - Discord 用户 id 绑定文件路径
-qq_bind_file = "data/discord_message_bridge_qq_bind.json"
-qq_bind_file_2 = "data/discord_message_bridge_qq_bind_2.json"
+qq_bind_file = config.qq_bind_file
+qq_bind_file_2 = config.qq_bind_file_2
 
 # 转发回复消息时显示的原消息内容的最大长度
-MAX_REPLY_PREVIEW_LENGTH = 100
+MAX_REPLY_PREVIEW_LENGTH = config.max_reply_preview_length
 
 # 转发消息前缀
-PREFIX = "[Discord] "
+PREFIX = config.prefix
 
 # Bot 名称
-BOT_NAME = "Discord Message Bridge"
+BOT_NAME = config.bot_name
 
 # QQ 机器人命令名
-QQ_COMMAND = "dmb"
+QQ_COMMAND = config.qq_command
 
 # QQ 机器人命令前缀
-QQ_COMMAND_PREFIX = "."
+QQ_COMMAND_PREFIX = config.qq_command_prefix
 
 # Discord 机器人命令前缀
-DISCORD_COMMAND_PREFIX = "~"
+DISCORD_COMMAND_PREFIX = config.discord_command_prefix
 
 # QQ 转发机器人以用户身份调用其它机器人时发送消息的格式
 # 若其它机器人支持此功能，才可使用，否则请留空
 # {Q} 自动替换为用户对应的 QQ 号
 # {C} 自动替换为用户发送的内容
-QQ_SUDO_FORMAT = "/sudo {Q} {C}"
+QQ_SUDO_FORMAT = config.qq_sudo_format
